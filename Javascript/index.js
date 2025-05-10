113c37fba89656e0fa5d519300472dc6cc9c7f74
Faa-ez21 <faaezabubakar@gmail.com>
2025-05-10 23:05:35 +0000

    update

---
+++ Javascript/index.js
@@ -0,0 +1,15 @@
+function test() {
+  var a = "3";
+  var b = "8";
+
+  /****Do not change the code above  *****/
+  //Write your code on lines 7 - 9.
+  var c = "a";
+  var a = "b";
+  var b = "a";
+
+  /*** Do not change the code below ***/
+
+   comsole.log("a is " + a);
+   console.log("b is " + b);
+}
