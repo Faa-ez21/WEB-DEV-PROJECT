f399bc52fce680de8f4051ff095ec1d9aec7186d
Faa-ez21 <faaezabubakar@gmail.com>
2025-05-30 22:18:37 +0000

    update

--- Dicee Challenge - Completed/index.js
+++ Dicee Challenge - Completed/index.js
@@ -10,7 +10,7 @@ var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src", randomImageSource);
 
 
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+v randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
